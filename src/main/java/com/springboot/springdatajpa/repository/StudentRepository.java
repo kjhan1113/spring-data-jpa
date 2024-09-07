afed33ceb17fff8b7b7f0db58d25f6ba7173e98c
kjhan1113 <kjhan1113@gmail.com>
2024-09-08 02:15:00 +0930

    Add mapping methods to SQL

--- src/main/java/com/springboot/springdatajpa/repository/StudentRepository.java
+++ src/main/java/com/springboot/springdatajpa/repository/StudentRepository.java
@@ -3,5 +3,18 @@ package com.springboot.springdatajpa.repository;
 import com.springboot.springdatajpa.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    Optional<Student> findStudentByEmail(String email);
+
+    long countByLastName(String lastName);
+
+    List<Student> findByAgeLessThanEqual(int age);
+    List<Student> findStudentsByFirstNameAndAgeEquals(String firstName, Integer age);
+    List<Student> findByLastNameOrderByLastNameAsc(String lastName);
+    List<Student> findByLastNameAndAgeOrderByFirstNameAsc(String lastName, int age);
+    List<Student> findByEmailContaining(String keyword); // like query
+    List<Student> findByAgeBetween(int startAge, int endAge); // between query
 }
